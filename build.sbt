4adc76b0b80ab6f571073b54b9b4c2da860e419d
Marcela Castro León <mcastrol@gmail.com>
2019-07-10 17:04:54 +0200

    first commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "PurchasesAndReturns"
+
+version := "0.1"
+
+scalaVersion := "2.13.0"
+
+
---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,6 @@
+package example
+
+object Main extends App {
+  val ages = Seq(42, 75, 29, 64,87)
+  println(s"The oldest person is ${ages.max}")
+}
